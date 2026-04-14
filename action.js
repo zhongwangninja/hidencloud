61b3e5e7421fac750a8488b53ab53f4078ae5895
zhongwang <zhongwangninja@gmail.com>
2026-04-14 08:19:04 +0800

    feat: renew by 7

--- action.js
+++ action.js
@@ -15,7 +15,7 @@ const os = require('os');
 // Enable stealth plugin
 chromium.use(stealth);
 
-const RENEW_DAYS = 10;
+const RENEW_DAYS = 1;
 const INJECTED_SCRIPT = `
 (function() {
     if (window.self === window.top) return;
